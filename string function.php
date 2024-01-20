ebdc3d9d9476b5318f9ff0573c74bd845e2c8313
vijay262000 <121922341+vijay262000@users.noreply.github.com>
2024-01-20 11:36:18 +0530

    Add files via upload

---
+++ string function.php
@@ -0,0 +1,18 @@
+<?php
+$name = "vijay is good boy";
+echo $name;
+echo "<br>";
+echo "Then length of my string is ". strlen($name);
+echo "<br>";
+echo str_word_count($name);
+echo "<br>";
+echo strrev($name);
+echo "<br>";
+echo strpos($name,"is");
+echo "<br>";
+echo str_replace("vijay","vaibhav",$name);
+echo "<br>";
+echo str_repeat($name,230);
+echo "<br>";
+
+?>
